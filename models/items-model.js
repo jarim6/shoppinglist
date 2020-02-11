5c862050d48bf9f4bda174a57a0b243d0e9aaee5
jarimutanen <jari.mutanen@gmail.com>
2020-02-11 18:13:11 +0200

    initial commit

---
+++ models/items-model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const item_schema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    users: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        req: true
+    }
+    notes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'item',
+        req: true
+    }]
+});
+const item_model = mongoose.model('item', item_schema);
+
+module.exports = item_model;
